aa6c578f3403517a321bf93d026b96b5a095e804
ttmc <madhusudhanbr06@gmail.com>
2022-05-09 10:59:08 +0530

    First commit

---
+++ Day2_java/src/Example1.java
@@ -0,0 +1,18 @@
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		int a=15;
+		float b=10.14f;
+		double c=15.1;
+		char d='b';
+		String str="Madhusudhan";
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(str);
+
+	}
+
+}
