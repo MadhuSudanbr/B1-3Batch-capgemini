e39a8e49d7e9ab6391afcb6c2d3c7375062f6ec2
ttmc <madhusudhanbr06@gmail.com>
2022-05-13 15:23:38 +0530

    First cpmmit

---
+++ Day6_Abstract_final_Super/src/abstract_capgemini/Example2.java
@@ -0,0 +1,11 @@
+package abstract_capgemini;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int a=20;
+        System.out.println(a);
+	}
+
+}
